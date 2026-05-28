f556475d47de616ccafa9dc367779976ba95d7da
Mathiya <mathiyathokozanin@gmail.com>
2026-05-28 09:05:03 +0200

    Completed JS exercises 06 - 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    if (array.length <= 0 || elementsToRemove.length <= 0) {
+        return array;
+    }
+    return array
+    .filter(element => !(elementsToRemove.includes(element)));
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
